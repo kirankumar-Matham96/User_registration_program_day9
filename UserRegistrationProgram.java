89c3f0550da7cf2ffae1c21caaaa80b620968463
kirankumar-Matham96 <mathamkirankumar96@gmail.com>
2021-03-31 16:54:42 +0530

    ADD: Validating phone number of the user

--- UserRegistrationProgram.java
+++ UserRegistrationProgram.java
@@ -1,3 +1,4 @@
+//import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserRegistrationProgram
@@ -20,33 +21,26 @@ public class UserRegistrationProgram
 		return Pattern.matches("[-a-zA-Z0-9+]+.[-a-zA-Z0-9]*@[a-zA-Z0-9]+(.[a-zA-Z0-9]+)*.[a-zA-Z]{2,}$", email);
 	}
 
+	//UC-4: validating mobile number
+	public static boolean validatePhoneNumber(String phoneNumber)
+	{
+		return Pattern.matches("^[91]{2}[0-9]{10}",phoneNumber);
+	}
+
 	//main method
 	public static void main(String[] args)
 	{
 		String firstName = "Rahul";
 		String lastName = "Shinde";
-		String email1 = "Rahul.Shinde@gmail.com.in";
-		String email2 = "Rahul.Shinde@gmail.com";
-		String email3 = "Rahul-Shinde@gmail.com";
-		String email4 = "Rahul.123@gmail.com";
-		String email5 = "Rahul.Shinde123@gmail.com";
-		String email6 = "RahulShinde.123@gmail.com";
-		String email7 = "Rahul123.Shinde123@gmail.com";
-		String email8 = "Rahul123.Shinde123@gmail.com.com";
-		String email9 = "Rahul123.Shinde123@yahoo.com.in";
-		String email10 = "Rahul-123.Shinde-123@rahul.com.in";
+		String email = "Rahul.Shinde@gmail.com.in";
+		long phoneNumber = 918688332960L;
 
-		System.out.println(validateFirstName(firstName));
-		System.out.println(validateLastName(lastName));
-		System.out.println(validateEmail(email1));
-		System.out.println(validateEmail(email2));
-		System.out.println(validateEmail(email3));
-		System.out.println(validateEmail(email4));
-		System.out.println(validateEmail(email5));
-		System.out.println(validateEmail(email6));
-		System.out.println(validateEmail(email7));
-		System.out.println(validateEmail(email8));
-		System.out.println(validateEmail(email9));
-		System.out.println(validateEmail(email10));
+		System.out.println("first name: "+validateFirstName(firstName));
+		System.out.println("last name: "+validateLastName(lastName));
+		System.out.println("email: "+validateEmail(email));
+
+		//converted long into string before passing phone number
+		System.out.println("phone numbar: "+validatePhoneNumber(Long.toString(phoneNumber)));
+
 	}
 }
