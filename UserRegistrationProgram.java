da34b2a36ad7e79e2f6f0e731ddf67baeb304d88
kirankumar-Matham96 <mathamkirankumar96@gmail.com>
2021-03-31 13:55:17 +0530

    ADD: Validating last name of the user

--- UserRegistrationProgram.java
+++ UserRegistrationProgram.java
@@ -1,16 +1,25 @@
 import java.util.regex.Pattern;
+
 public class UserRegistrationProgram
 {
 	//UC-1: validating first name
 	public static boolean validateFirstName(String firstName)
 	{
-	      return Pattern.matches("^[A-Z]{1}[a-z]{2,}", firstName);
+		return Pattern.matches("^[A-Z]{1}[a-z]{2,}", firstName);
+	}
+
+	//UC-2: validating last name
+	public static boolean validateLastName(String lastName)
+	{
+		return Pattern.matches("^[A-Z]{1}[a-z]{2,}", lastName);
 	}
 
 	//main method
 	public static void main(String[] args)
 	{
 		String firstName = "Rahul";
+		String lastName = "Shinde";
 		System.out.println(validateFirstName(firstName));
+		System.out.println(validateLastName(lastName));
 	}
 }
